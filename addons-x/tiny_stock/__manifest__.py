e4742ae45071e515ae99f31e5b541c8d67c81006
KullyFromHome <nexus5onlykully@gmail.com>
2020-11-04 18:22:06 +0700

    edit view m_item

--- addons-x/tiny_stock/__manifest__.py
+++ addons-x/tiny_stock/__manifest__.py
@@ -24,8 +24,10 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/m_item.xml',
         'views/views.xml',
         'views/templates.xml',
+        
     ],
     # only loaded in demonstration mode
     'demo': [
